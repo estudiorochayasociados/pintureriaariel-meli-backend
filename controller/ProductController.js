e252846f8e791e7e4212c1d1bea296540eb28012
FACUNDO ROCHA <facundo@estudiorochayasoc.com.ar>
2020-02-07 11:10:45 -0300

    a

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 const ProductsModel = require('../model/ProductModel');
-const {removeSpecialChars} = require('../controller/HelpersController');
+const { removeSpecialChars } = require('../controller/HelpersController');
 
-exports.updateProductsWithWeb = async (link) => { 
+exports.updateProductsWithWeb = async (link) => {
     var add = [];
     var update = [];
     var totalArray = [];
@@ -10,12 +10,12 @@ exports.updateProductsWithWeb = async (link) => {
     return axios.get(link)
         .then(async r => {
             for await (const item of r.data) {
-                var itemSearch = await this.view(item.data.cod_producto);                
+                var itemSearch = await this.view(item.data.cod_producto);
                 if (itemSearch) {
                     const images = [];
                     itemSearch.title = removeSpecialChars(item.data.titulo);
                     itemSearch.description.text = item.data.desarrollo;
-                    itemSearch.description.video =  (item.data.video) ? item.data.video : process.env.VIDEO_ITEM;
+                    itemSearch.description.video = (item.data.video) ? item.data.video : process.env.VIDEO_ITEM;
                     itemSearch.stock = (item.data.stock) ? item.data.stock : 0;
                     itemSearch.code.web = item.data.cod_producto;
                     // itemSearch.mercadolibre = item.mercadolibre;
@@ -27,7 +27,7 @@ exports.updateProductsWithWeb = async (link) => {
                     });
                     itemSearch.images = images;
                     this.update(itemSearch);
-                    update.push({product:itemSearch.title});
+                    update.push({ product: itemSearch.title });
                 } else {
                     const data = {};
                     data.description = {};
@@ -36,7 +36,7 @@ exports.updateProductsWithWeb = async (link) => {
                     const images = [];
                     data.title = removeSpecialChars(item.data.titulo);
                     data.description.text = item.data.desarrollo;
-                    data.description.video =  (data.description.video) ? data.description.video :process.env.VIDEO_ITEM;
+                    data.description.video = (data.description.video) ? data.description.video : process.env.VIDEO_ITEM;
                     data.stock = (item.data.stock) ? item.data.stock : 0;
                     data.code.web = item.data.cod_producto;
                     // data.mercadolibre = item.mercadolibre;
@@ -48,12 +48,63 @@ exports.updateProductsWithWeb = async (link) => {
                     });
                     data.images = images;
                     this.create(data);
-                    add.push({product:data.title});                    
+                    add.push({ product: data.title });
                 }
-            } 
-            totalArray.push({"add" : add, "update" : update});
+            }
+            totalArray.push({ "add": add, "update": update });
             return await totalArray;
-        }) 
+        })
+}
+
+
+exports.updateWeb = async (item) => {
+    this.setStock(0);
+    var itemSearch = await this.view(item.data.cod_producto);
+    if (itemSearch) {
+        const images = [];
+        itemSearch.title = removeSpecialChars(item.data.titulo);
+        itemSearch.description.text = item.data.desarrollo;
+        itemSearch.description.video = (item.data.video) ? item.data.video : process.env.VIDEO_ITEM;
+        itemSearch.stock = (item.data.stock) ? item.data.stock : 0;
+        itemSearch.code.web = item.data.cod_producto;
+        // itemSearch.mercadolibre = item.mercadolibre;
+        itemSearch.price.default = item.data.precio;
+        itemSearch.category = removeSpecialChars(item.category.data.titulo);
+        itemSearch.subcategory = removeSpecialChars(item.subcategory.data.titulo);
+        item.images.forEach(img => {
+            images.push({ "source": img.ruta, "order": img.orden })
+        });
+        itemSearch.images = images;
+        if (this.update(itemSearch)) {
+            return ({ status: 200, type: "update", title: item.data.titulo });
+        } else {
+            return ({ status: 500, type: "update", title: item.data.titulo });
+        }
+    } else {
+        const data = {};
+        data.description = {};
+        data.code = {};
+        data.price = {};
+        const images = [];
+        data.title = removeSpecialChars(item.data.titulo);
+        data.description.text = item.data.desarrollo;
+        data.description.video = (data.description.video) ? data.description.video : process.env.VIDEO_ITEM;
+        data.stock = (item.data.stock) ? item.data.stock : 0;
+        data.code.web = item.data.cod_producto;
+        // data.mercadolibre = item.mercadolibre;
+        data.price.default = item.data.precio;
+        data.category = removeSpecialChars(item.category.data.titulo);
+        data.subcategory = removeSpecialChars(item.subcategory.data.titulo);
+        item.images.forEach(img => {
+            images.push({ "source": img.ruta, "order": img.orden })
+        });
+        data.images = images;
+        if (this.create(itemSearch)) {
+            return ({ status: 200, type: "create", title: item.data.titulo });
+        } else {
+            return ({ status: 500, type: "create", title: item.data.titulo });
+        }
+    }
 }
 
 exports.list = async () => {
@@ -79,6 +130,6 @@ exports.view = function (codigo) {
     return ProductsModel.findOne({ 'code.web': codigo }, (err, res) => { return res });
 };
 
-exports.setStock = function(stock) {
+exports.setStock = function (stock) {
     return ProductsModel.updateMany({}, { stock: stock })
 }
--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -1,31 +1,37 @@
 const express = require('express');
-const config  = require('dotenv').config();
+const config = require('dotenv').config();
 const Middelware = require("../config/Middleware");
 const ProductController = require('../controller/ProductController');
 const router = express.Router();
 
-router.get("/update-products-with-web",  async (req, res) => {
+router.get("/update-products-with-web", async (req, res) => {
     var get = await ProductController.updateProductsWithWeb(config.parsed.JSON_PRODUCT);
     res.send(get);
 })
 
-router.get("/",Middelware.checkToken, async (req, res) => {
+
+router.post("/update-web", Middelware.checkToken, async (req, res) => {
+    const get = await ProductController.updateWeb(req.body.item);
+    res.status(200).send(get);
+})
+
+router.get("/", Middelware.checkToken, async (req, res) => {
     const get = await ProductController.list();
     res.status(200).send(get);
 })
 
-router.get("/:id",Middelware.checkToken, async (req, res) => {
+router.get("/:id", Middelware.checkToken, async (req, res) => {
     console.log(req.params.id);
     let view = await ProductController.view(req.params.id);
     res.status(200).json(view);
 })
 
-router.post("/",Middelware.checkToken, async (req, res) => {
+router.post("/", Middelware.checkToken, async (req, res) => {
     const get = await ProductController.create(req.body);
     res.status(200).send({ get });
 })
 
-router.put("/",Middelware.checkToken, async (req, res) => {
+router.put("/", Middelware.checkToken, async (req, res) => {
     const get = await ProductController.update(req.body);
     res.status(200).send({ get });
 })
